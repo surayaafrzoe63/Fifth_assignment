8d81b62d8e6a3854de1e53818670ee859d0ef458
surayaafroze63 <surayaafroze63@gmail.com>
2026-03-07 09:07:26 +0600

    added login page

---
+++ scripts/login.js
@@ -0,0 +1,15 @@
+document.getElementById('login-btn').addEventListener('click', function () {
+  const inputName = document.getElementById('input-name')
+  const show = inputName.value
+  console.log(show)
+const pinInput=document.getElementById('input-pin')
+const pin=pinInput.value
+console.log(pin)
+
+if(show=='admin' && pin=='admin123'){
+ alert('Login success')
+ window.location.assign('/home.html')
+// window.location.assign('/h')
+}
+else{alert('Login failed')}
+})
