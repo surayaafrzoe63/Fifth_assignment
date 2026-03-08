811de9710f67b1ec6dbca17373a95a82b627f42b
surayaafroze63 <surayaafroze63@gmail.com>
2026-03-09 01:07:10 +0600

    added issues count system and and fixed many problem and also make proper card

--- scripts/script.js
+++ scripts/script.js
@@ -1,51 +1,136 @@
-// const button=document.querySelectorAll(".filter-btn")
-// button.forEach(btn=>{
-//   btn.addEventListener("click",()=>{
-//     button.forEach(b=>{
-//       b.classList.remove("bg-primary", "text-white");
-      
-//       b.classList.add("text-[#64748B]");
-
-//     })
-//      btn.classList.add("bg-primary", "text-white");
-      
-//       btn.classList.remove("text-[#64748B]");
-
-//   })
-
-// })
+let alldata=[]
+function loadData(url){
+  fetch(url)
+  .then((res)=>res.json())
+  .then(data=>{
+    alldata=data.data;
+    displayData(alldata)
+  })
+}
+loadData("https://phi-lab-server.vercel.app/api/v1/lab/issues")
 
 // toggeling
-const allFilterBtn=document.getElementById("all-btn")
-const openFilterBtn=document.getElementById("open-btn")
-const closedFilterBtn=document.getElementById("closed-btn")
+const allFilterBtn = document.getElementById("all-btn")
+const openFilterBtn = document.getElementById("open-btn")
+const closedFilterBtn = document.getElementById("closed-btn")
 
-allFilterBtn.addEventListener("click",()=>{
+
+allFilterBtn.addEventListener("click", () => {
   allFilterBtn.classList.add("bg-primary", "text-white")
   openFilterBtn.classList.remove("bg-primary", "text-white")
   closedFilterBtn.classList.remove("bg-primary", "text-white")
+  displayData(alldata)
 })
-openFilterBtn.addEventListener("click",()=>{
+openFilterBtn.addEventListener("click", () => {
   openFilterBtn.classList.add("bg-primary", "text-white")
   allFilterBtn.classList.remove("bg-primary", "text-white")
   closedFilterBtn.classList.remove("bg-primary", "text-white")
+
+   const openItems = alldata.filter(item => item.priority === "high" || item.priority === "medium")
+  displayData(openItems)
 })
-closedFilterBtn.addEventListener("click",()=>{
+closedFilterBtn.addEventListener("click", () => {
   closedFilterBtn.classList.add("bg-primary", "text-white")
   allFilterBtn.classList.remove("bg-primary", "text-white")
-   openFilterBtn.classList.remove("bg-primary", "text-white")
+  openFilterBtn.classList.remove("bg-primary", "text-white")
+  const closedItems = alldata.filter(item => item.priority === "low")
+  displayData(closedItems)
 })
 
 
 // json formate
-function loadData (url){
-  fetch(url)
-  .then((res)=>res.json())
-  .then((data)=>displayData(data.data))
-}
-loadData("https://phi-lab-server.vercel.app/api/v1/lab/issues")
 
-function displayData(items){
-  const cardContainer=document.getElementById('card-container')
-    console.log(items);}
+function displayData(items) {
+  const allContainer = document.getElementById('all-container')
+  const issueCount = document.getElementById("issue-count")
+
+
+  allContainer.innerHTML =""
+  issueCount.innerText = items.length + " Issues"
+  console.log(items);
+  items.forEach((item) => {
+    //priority color change
+    let priorityColor = ""
+    let image = ""
+    
+    if (item.priority === "high") {
+      priorityColor = "bg-[#FEECEC] text-[#EF4444]"
+      image = "assets/Open-Status.png"
+      
+    }
+    else if (item.priority === "medium") {
+      priorityColor = "bg-[#FFF6D1] text-[#F59E0B]"
+      image = "assets/Open-Status.png"
+      
+    }
+    else {
+      priorityColor = "bg-[#EEEFF2] text-[#9CA3AF]"
+        image = "assets/Status.png"}
+  
+    const div1 = document.createElement("div")
+    div1.className = "card bg-base-100  shadow-sm p-2 h-full ${ border} "
+    div1.innerHTML = `
+          <div class="flex justify-between">
+            <img  src="${image}" alt="">
+            <div class="badge badge-error  border-none  ${priorityColor}">${item.priority}
+            </div>
+          </div>
+
+      
+          <div class="card-body text-left">
+            <h2 class="text-[#1F2937] text-xl font-semibold text-[14px] line-clamp-2">
+              ${item.title}
+            </h2>
+            <p class="text-[#64748B] text-xl text-[12px] line-clamp-3">${item.description}
+            </p>
+          </div>
+
+
+        
+
+            <div class="flex gap-2  border-b-2 border-gray-300 mb-2 pb-3">
+            ${item.labels
+        .map((label) => {
+         let labelColor="" 
+if(label === "bug"){
+  labelColor = "bg-[#FEECEC] text-[#EF4444]"
+ 
+}
+else if(label === "help wanted"){
+  labelColor = "bg-[#FFF6D1] text-[#F59E0B]"
+ 
+}
+else if(label === "enhancement"){
+  labelColor = "bg-[#DEFCE8] text-[#00A96E]"
+ 
+
+}
+else{
+  labelColor = "bg-[#FEECEC] text-[#EF4444]";
+  image="assets/Vector (3).png"
+}
+
+         
+          return `<span  class="badge badge-outline ${labelColor}">
+        
+      ${label}
+    </span>`
+        })
+        .join("")
+      }
+            </div>
+          <div class="space-y-1.5">
+            <p class="text-[#64748B] text-[12px]">
+              ${item.author}
+            </p>
+            <p class="text-[#64748B] text-[12px] ">
+              ${item.createdAt}
+            </p>
+          </div>
+  `
+    allContainer.appendChild(div1)
+  })
+
+}
+
 
